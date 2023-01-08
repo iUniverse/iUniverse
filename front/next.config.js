3084774993ecd8f94e9298e32ff748f115011229
JungIn <skso789@naver.com>
2023-01-08 23:12:04 +0900

    디렉토리 구조 설정 tmp 파일은 나중에 삭제

--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,37 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = (phase, {defaultConfig}) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      /* development only config options here */
+
+    }
+  }
+
+  return {
+    nextConfig,
+    /* config options for all phases except development here */
+    env: {
+      /**
+       * .env랑 여기랑 차이는 없으나 관리의 편의성을 위해 .env 사용 (.env가 동작 안하면 'dotenv'모듈 알아보기)
+       * 'NEXT_PUBLIC_' 앞에 붙이면 네트워크 상에서 유출이 안됨 (추후 웹 개발자모드 터미널에서 쳐보면 알수 있다.)
+       * */
+    },
+    webpack: {
+      /**
+       * nextjs는 자체 웹팩 및 번들링을 제공해줌...
+       * 사용법은 추후에;;
+       * entry, output만 사용한 번들링 custom이 필요하면 여기 추가;
+       */
+
+    }
+
+
+  }
+}
+
