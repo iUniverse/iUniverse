3de8153c884b34051275755f2043e7810ced97b2
김태호 <>
2023-01-10 02:21:30 +0900

    typeORM - postgre 셋팅

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeORMConfig)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
---
+++ back/src/configs/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { TypeOrmModuleOptions  } from "@nestjs/typeorm";
+
+
+export const typeORMConfig : TypeOrmModuleOptions = {
+    type : 'postgres',
+    host : 'localhost',
+    port : 5432,
+    username : 'postgres',
+    password : 'emth0229',
+    database : 'board-app',
+    entities : [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize : true, 
+    /**
+     * synchronize 주의 사항
+     * 해당 기능은 어플리케이션을 다시 실행할 때, entity 안에서 수정된 칼럼의 길이, 타입 변경값등을 
+     * 해당 테이블을 drop한 후 다시 생성해준다. 
+     * 테스트용은 true로 하되 실제 서비스를 실행할 경우 false로 한다.
+     */
+}
