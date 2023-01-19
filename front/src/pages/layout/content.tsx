49b584c88d6780d9816831acf6b288a9fe929c72
김태호 <>
2023-01-19 19:28:48 +0900

    iUniLayout 만듬

---
+++ front/src/pages/layout/content.tsx
@@ -0,0 +1,20 @@
+import { Layout, Menu, theme } from 'antd';
+const { Header, Sider, Content } = Layout;
+
+export default function IUniContent(props : any){
+    const { token: { colorBgContainer }, } = theme.useToken();
+    return(
+        <>
+            <Content
+                style={{
+                margin: '24px 16px',
+                padding: 24,
+                minHeight: '30vh',
+                background: colorBgContainer,
+                }}
+            >
+                {props.page}
+            </Content>
+        </>
+    )
+}
