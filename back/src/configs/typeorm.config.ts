45bee36ff6655ac14ff4e042eb6ff9f2a5ec06a8
yejin <>
2023-01-30 19:51:13 +0900

    db pw변경

--- back/src/configs/typeorm.config.ts
+++ back/src/configs/typeorm.config.ts
@@ -5,7 +5,7 @@ export const typeORMConfig : TypeOrmModuleOptions = {
     host : 'localhost',
     port : 5432,
     username : 'postgres',
-    password : 'emth0229',
+    password : '1235',
     database : 'iuniverse',
     entities : [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize : true, 
