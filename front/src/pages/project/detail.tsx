ec9ab1ac974115be327278c9fea843c04294bae9
김태호 <>
2023-02-06 21:08:54 +0900

    프로젝트 상세보기 - ant design 추가 및 상세조회 데이터 보이지 않는 문제 수정

--- front/src/pages/project/detail.tsx
+++ front/src/pages/project/detail.tsx
@@ -1,9 +1,11 @@
-import { Col, Row } from "antd";
+import { Col, Form, Row, Input, Card, DatePicker, Switch, Select } from "antd";
 import { getProject } from "api/project/project";
 import { useRouter } from 'next/router';
 import { useEffect, useState } from "react";
 
-export default function Detail(props : any){    
+const { RangePicker } = DatePicker;
+export default function Detail(props : any){  
+      
     const router = useRouter();
     const id : number = Number(router.query['id']);
     const [project, setProject] = useState({});
@@ -14,6 +16,7 @@ export default function Detail(props : any){
 
     async function find(id : number) : Promise<void>{
         let project = await getProject(id);
+        console.log(project);
         setProject(() => project);
     }
 
@@ -23,16 +26,50 @@ export default function Detail(props : any){
 
 
     return(
-        <Row gutter={[16, 16]}>
-            <Col span={24}>
-                <h3>프로젝트 이름 : {project.name}</h3>
-            </Col>
-            <Col span={24}>
-                <h3>프로젝트 내용 : {project.description}</h3>
-            </Col>
-            <Col span={24}>
-                <h3>프로젝트 생성날짜 : {project.createDate}</h3>
-            </Col>
-        </Row>
+        <>
+            <Form
+                labelCol = {{span:4}}
+                wrapperCol={{span :14}}
+                layout="horizontal"
+                style = {{maxWidth:600}}
+            >
+                <Card>
+                    <Form.Item label="프로젝트">
+                        <Input value={project.name} />
+                    </Form.Item>
+                    <Form.Item label="내용">
+                        <Input value={project.description} />
+                    </Form.Item>
+                    <Form.Item label="생성날짜">
+                        <Input value={project.createDate} />
+                    </Form.Item>
+                    <Form.Item label="색상">
+
+                    </Form.Item>
+                    <Form.Item label="프로젝트 기간">
+                        <RangePicker />
+                    </Form.Item>              
+                    <Form.Item label="이행날짜">
+                        <DatePicker />
+                    </Form.Item>
+                    <Form.Item label="진행률">
+                    </Form.Item> 
+                    <Form.Item label="상태">
+                        <Select>
+                            <Select.Option value="0">대기중</Select.Option>
+                            <Select.Option value="1">진행중</Select.Option>
+                            <Select.Option value="2">완료</Select.Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label="타입">
+                    </Form.Item>
+                    <Form.Item label="생성자">
+                    </Form.Item>
+                    <Form.Item label="공개 여부">
+                        <Switch />
+                    </Form.Item>
+                </Card>                      
+            </Form>
+        </>
     );
 }
