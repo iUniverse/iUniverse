0238c1669ec268296fd2075df19d79cc950f9f30
tom <tom@heaventree.co.kr>
2023-06-02 17:14:56 +0900

    공통 select 진행중

--- front/src/pages/layout/iuniSelect.tsx
+++ front/src/pages/layout/iuniSelect.tsx
@@ -1,34 +1,48 @@
 import { useRef, useState } from "react";
 
 interface Props {
-    optionList : Array<any>;
-    defaultChecked : string | number | undefined;
+    optionList: any[];
+    defaultChecked: string | number | undefined;
 
 }
-export default function IuniSelect(props : Props){
+export default function IuniSelect(props: Props) {
     /* 선택 되어 있는 옵션값 */
-    const [selectId, setSelectId] = useState<any>();
+    const [selectVal, setSelectVal] = useState<any>(props.optionList.find(z => z.id === props.defaultChecked));
     /* 옵션 ref */
     const optionRef = useRef<any>([]);
-    const [optionListState , setOptionListState] = useState<boolean>(false);
+    const [optionListState, setOptionListState] = useState<boolean>(false);
     const toggleOptionList = () => setOptionListState(() => !optionListState);
-
-    return(
+    const handleOption = (val: any) => {
+        setSelectVal(() => val);
+    }
+    return (
         <>
-            <div style={{width : '100%', height :'48px'}} onClick={() => toggleOptionList()}>
-                테마 설정
+            <div className="iuni-select-box" onClick={() => toggleOptionList()}>
+                <p>
+                    <>
+                        {
+                            selectVal.name
+                        }
+                    </>
+                </p>
             </div>
-            <div className="iuni-select-option-list">
-                <div className="iuni-select-option">
-                    내 정보 변경
+
+            {
+                optionListState === true &&
+                <div className="iuni-select-option-list">
+                    {
+                        props.optionList.map((val: any, index) => (
+                            <>
+                                <div className={selectVal.id === val.id ? 'iuni-select-option f-bold' : 'iuni-select-option f-normal'}
+                                    key={val.id}
+                                    onClick={() => handleOption(val)}>
+                                    <p>{val.name}</p>
+                                </div>
+                            </>
+                        ))
+                    }
                 </div>
-                <div className="iuni-select-option">
-                    테마 설정
-                </div>
-                <div className="iuni-select-option">
-                    캐릭터 설정
-                </div>
-            </div>
+            }
         </>
     )
 }
--- front/src/pages/theme/index.tsx
+++ front/src/pages/theme/index.tsx
@@ -8,6 +8,7 @@ import { loadMyThemeInfo } from 'api/theme/card-theme';
 import Modal from 'react-modal';
 import IuniAlert from 'pages/layout/iuniAlert';
 import { throttle } from 'lodash';
+import IuniSelect from 'pages/layout/iuniSelect';
 
 export interface ThemeInfo {
     id: number;
@@ -92,12 +93,25 @@ export default function Index() {
     }, []);
     useEffect(() => {
         resizeObserver();
-    }, [themeId])
+    }, [themeId]);
+
+    const test = [
+        {'id' : 0, 'name' : '내 정보 변경'},
+        {'id' : 1, 'name' : '테마 설정'},
+        {'id' : 2, 'name' : '캐릭터 설정'},
+    ];
+
     return (
         <>
             <div className="theme-setting-container">
                 <div className="theme-setting-menu">
                     <SubSideMenu currentSize={currentSize} />
+                </div>
+                <div>
+                    <IuniSelect 
+                        optionList={test}
+                        defaultChecked={test[0].id}
+                    />
                 </div>
                 <div className="theme-setting-content mt-3r">
                     <div className="theme-setting-content-title">
