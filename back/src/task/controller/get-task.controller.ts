970857706457d2288ddc3bd711f934e898b00325
김태호 <>
2023-06-13 01:51:58 +0900

    task 칸반작업 진행중, task back 기능 추가 및 수정 작업 진행중

--- back/src/task/controller/get-task.controller.ts
+++ back/src/task/controller/get-task.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get, Inject } from '@nestjs/common';
-import { LoadTaskInboundPort, LoadTaskInboundPortOutputDto, LOAD_TASK_INBOUND_PORT } from '../inbound-port/load-task.inbound-port';
+import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { LoadTaskInboundPort, LoadTaskInboundPortOutputDto, LOAD_TASK_INBOUND_PORT, LoadTaskIPInputDto } from '../inbound-port/load-task.inbound-port';
 
 @Controller('iuni_task')
 export class GetTaskController {
     constructor(
         @Inject(LOAD_TASK_INBOUND_PORT)
         private readonly loadTaskInboundPort: LoadTaskInboundPort
-    ){}
-    
-    @Get('/')
-    async load():Promise<LoadTaskInboundPortOutputDto>{
-        return this.loadTaskInboundPort.load()
+    ) {}
+
+    @Get('/:id')
+    async load(@Param() param: LoadTaskIPInputDto): Promise<LoadTaskInboundPortOutputDto> {
+        try {
+            console.log("애아니;ㅓ미나ㅓㅇ림나?");
+            console.log(param);
+            return this.loadTaskInboundPort.load(param);
+        }
+        catch (e) {
+            console.log(e)
+            throw e;
+        }
+
     }
 }
